b9202b9280b9df0f1a48d9005e93bb2279964775
AnuragIncerto <anurag@incerto.in>
2025-10-24 02:20:59 +0530

    add: template and context

--- app/AI-Basics/_meta.js
+++ app/AI-Basics/_meta.js
@@ -2,5 +2,4 @@ export default {
   "LLM-Basics": "LLM Basics",
   "Agents-Basics": "Agents Basics",
   "Context-Basics": "Context Basics",
-  Context: "Context",
 };
--- app/_meta.js
+++ app/_meta.js
@@ -1,6 +1,8 @@
 export default {
   "get-started" : "Get Started",
-  "AI-Basics" : "AI Basics"
+  "AI-Basics" : "AI Basics",
+  "tutorial": "Tutorials",
+  "database": "Database",
   // resources: "Resources 🔥",
   // "###": {
   //   type: "separator",
